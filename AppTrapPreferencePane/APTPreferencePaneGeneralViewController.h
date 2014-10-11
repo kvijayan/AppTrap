d980b681625009b2cb8b55bd367a4f567f145a2e
Kumaran Vijayan <vijayan@kumaran.ca>
2014-10-11 13:05:50 -0600

    - Added layout constraints to the general tab view

---
+++ AppTrapPreferencePane/APTPreferencePaneGeneralViewController.h
@@ -0,0 +1,13 @@
+//
+//  APTPreferencePaneGeneralViewController.h
+//  AppTrapPreferencePane
+//
+//  Created by Kumaran Vijayan on 2014-10-11.
+//
+//
+
+@import AppKit.NSViewController;
+
+@interface APTPreferencePaneGeneralViewController : NSViewController
+
+@end
---
+++ AppTrapPreferencePane/APTPreferencePaneGeneralViewController.m
@@ -0,0 +1,32 @@
+//
+//  APTPreferencePaneGeneralViewController.m
+//  AppTrapPreferencePane
+//
+//  Created by Kumaran Vijayan on 2014-10-11.
+//
+//
+
+#import "APTPreferencePaneGeneralViewController.h"
+
+@implementation APTPreferencePaneGeneralViewController
+
+- (void)setUpLocalizationStrings
+{
+    
+}
+
+- (void)viewDidLoad
+{
+    [self setUpLocalizationStrings];
+}
+
+#pragma mark - NSViewController
+
+- (void)loadView
+{
+    [super loadView];
+// TODO: Remember to support 10.10's viewDidLoad method implementation
+    [self viewDidLoad];
+}
+
+@end
